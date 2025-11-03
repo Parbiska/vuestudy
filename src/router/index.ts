698c6255ecc341784690ea8172c8fbbec355379d
Demid Zaynulin <demid.zaynulin@machinists.ru>
2025-11-04 00:39:16 +0800

    add basic pages for Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/favorites',
+      name: 'favorites',
+      component: () => import('@/views/FavoritesView.vue')
+    },
+    {
+      path: '/coin/:id',
+      name: 'coin-detail',
+      component: () => import('@/views/CoinDetailView.vue')
+    }
+  ],
 })
 
 export default router
