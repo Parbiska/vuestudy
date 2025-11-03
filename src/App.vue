cb2938e2cd5fef0e335eb3de58fcd9beeaa17c1c
Demid Zaynulin <demid.zaynulin@machinists.ru>
2025-11-04 01:22:34 +0800

    rename Header/Footer to AppHeader/AppFooter to follow Vue multi-word component naming rule

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <Header />
+    <AppHeader />
     
     <main class="flex-1 container mx-auto px-4 py-6">
       <RouterView />
     </main>
     
-    <Footer />
+    <AppFooter />
   </div>
 </template>
 
